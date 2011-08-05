3ccb6fff9607d546fd2fc25f7f3fedc033345a90
Valentin Nechitailenko <valnech.m@gmail.com>
2011-08-05 14:52:31 +0300

    admin users should be approved

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -5,7 +5,7 @@ class Admin < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :approved
 
   validates_confirmation_of :password
   validates_format_of :email,
