0579298fb9c8e1dddd0be9f8653a9aac8a9763e0
Valentin Nechitailenko <valnech.m@gmail.com>
2011-09-20 12:41:33 +0300

    Production

---
+++ config/setup_load_paths.rb
@@ -0,0 +1,20 @@
+if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
+  begin
+    rvm_path     = File.dirname(File.dirname(ENV['MY_RUBY_HOME']))
+    rvm_lib_path = File.join(rvm_path, 'lib')
+    $LOAD_PATH.unshift rvm_lib_path
+    require 'rvm'
+    RVM.use_from_path! File.dirname(File.dirname(__FILE__))
+  rescue LoadError
+    # RVM is unavailable at this point.
+    raise "RVM ruby lib is currently unavailable."
+  end
+end
+# # Or Bundler 0.9...
+if File.exist?(".bundle/environment.rb")
+  require '.bundle/environment'
+else
+  require 'rubygems'
+  require 'bundler'
+  Bundler.setup
+end
