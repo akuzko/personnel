cf24c257fa92c217d923a4e3bf39da659808702f
Valentin Nechitailenko <valnech.m@gmail.com>
2011-10-03 12:20:08 +0300

    Late Comings in User Area - date filter

--- app/models/late_coming.rb
+++ app/models/late_coming.rb
@@ -19,20 +19,4 @@ class LateComing < ActiveRecord::Base
              :conditions => conditions.join(' and '),
              :order => params[:sort_by]
   end
-
-  def self.search_by_admin(params, admin_id)
-    params[:sort_by] ||= '`profiles`.last_name, `profiles`.first_name, `shifts`.shiftdate'
-
-    admin = Admin.find_by_id(admin_id)
-    conditions = []
-    conditions.push("`users`.department_id IN (#{admin.departments.map{|d|d.id}.join(',')})") unless admin.super_user?
-    conditions.push("`users`.department_id = '" + params[:department_id] + "'") unless params[:department_id].nil? || params[:department_id] == ""
-    conditions.push("`shifts`.user_id = '" + params[:user_id] + "'") unless params[:user_id].nil? || params[:user_id] == ""
-    conditions.push("`shifts`.shiftdate >= '" + params[:date_from].to_s + "'") unless params[:date_from].nil? || params[:date_from] == "" || params[:date_from_check].nil?
-    conditions.push("`shifts`.shiftdate <= '" + params[:date_to].to_s + "'") unless params[:date_to].nil? || params[:date_to] == "" || params[:date_to_check].nil?
-
-    paginate :per_page => params[:per_page], :page => params[:page],
-             :conditions => conditions.join(' and '),
-             :order => params[:sort_by]
-  end
 end
