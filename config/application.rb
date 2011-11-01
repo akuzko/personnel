2a006e876a117264601e14e22e4123d47f2d0fef
Valentin Nechitailenko <valnech.m@gmail.com>
2011-11-01 10:13:35 +0200

    time zone

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Personnel
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'Kyiv'
+    config.time_zone = 'Athens'
     config.active_record.default_timezone = :local
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
