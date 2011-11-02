3e58a85e4a80b5217bde719bfa841e019a6b0a44
Valentin Nechitailenko <valnech.m@gmail.com>
2011-11-02 16:30:42 +0200

    translate

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module Personnel
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :ru
+    config.i18n.default_locale = :en
 
     config.generators do |g|
       g.stylesheets false
