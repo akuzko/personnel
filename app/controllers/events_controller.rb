674ccc1322eddcc6739d35f45b6a565f0a1b1a92
valnech <valnech.m@gmail.com>
2012-06-18 20:27:53 +0300

    department_id filter

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -245,15 +245,16 @@ class EventsController < ApplicationController
   end
 
   def processed_by_person
-    if session[:shift_id]
-      shift = Shift.find_by_id session[:shift_id]
-      params[:date_from] = shift.schedule_start_time.to_formatted_s(:date_and_time) rescue nil
-    else
-      params[:date_from] = (Date.current - 1.month).to_formatted_s(:date_and_time) if !params[:date_from]
+    if !params[:date_from]
+      if session[:shift_id]
+        shift = Shift.find_by_id session[:shift_id]
+        params[:date_from] = shift.schedule_start_time.to_formatted_s(:date_and_time) rescue nil
+      else
+        params[:date_from] = (Date.current - 1.month).to_formatted_s(:date_and_time)
+      end
     end
-
     params[:date_to] = DateTime.current.to_formatted_s(:date_and_time) if !params[:date_to]
-
+    params[:department_id] = current_user.department_id.to_s
     params[:user_id] = (current_user.id).to_s unless current_user.team_lead?
 
     @events = Event.processed_by_person(params, 0)
